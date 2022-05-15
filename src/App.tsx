1b39a64cf05489b7c3c7172874f60afb8923cd1f
Dong Zhou <dozhou@rubiconproject.com>
2022-05-15 03:50:54 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,8 @@ import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import MenuIcon from '@mui/icons-material/Menu';
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  // ...theme.mixins.toolbar, // necessary for content to be below app bar
-  justifyContent: 'flex-end',
-}));
+import Side from './layouts/Side';
+import Main from "./layouts/Main";
 
 function App() {
   const [open, setOpen] = React.useState<boolean>(true);
@@ -27,49 +21,8 @@ function App() {
 
   return (
     <Box sx={{ display: "flex" }} style={{ height: "100%" }}>
-      <Drawer
-        sx={{
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            // width: 160,
-            // backgroundColor: " green"
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          <ListItem>
-            Dynamic Programming
-          </ListItem>
-          <ListItem>
-            Sorting
-          </ListItem>
-        </List>
-      </Drawer>
-
-
-      <Toolbar>
-        <IconButton
-          color="inherit"
-          onClick={handleDrawerOpen}
-          edge="start"
-        // sx={{ mr: 2, ...(open && { display: 'none' }) }}
-        >
-          <MenuIcon />
-        </IconButton>
-
-        <Typography variant="h6">
-          alchemist
-        </Typography>
-      </Toolbar>
+      <Side />
+      <Main />
     </Box>
   );
 }
