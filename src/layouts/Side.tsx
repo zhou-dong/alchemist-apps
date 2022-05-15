c6c7ead65c447c293b1b326563f254f738dd2b72
Dong Zhou <dozhou@rubiconproject.com>
2022-05-15 05:07:05 -0700

    Update Side.tsx

--- src/layouts/Side.tsx
+++ src/layouts/Side.tsx
@@ -1,9 +1,10 @@
 import { styled, useTheme } from '@mui/material/styles';
-import { Box, CssBaseline, Divider, Drawer, IconButton, List, ListItem, Toolbar, Typography } from "@mui/material";
+import { Box, CssBaseline, Divider, Drawer, IconButton, List, ListItem, ListItemButton, Toolbar, Typography } from "@mui/material";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import React from "react";
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import MenuIcon from '@mui/icons-material/Menu';
+import { green, grey } from '@mui/material/colors';
 
 const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -11,6 +12,31 @@ const DrawerHeader = styled('div')(({ theme }) => ({
     padding: theme.spacing(0, 1),
     ...theme.mixins.toolbar, // necessary for content to be below app bar
     justifyContent: 'flex-end',
+}));
+
+const drawerWidth = 120;
+
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
+}
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+    open?: boolean;
+}>(({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    }),
 }));
 
 export default () => {
@@ -31,8 +57,9 @@ export default () => {
                 sx={{
                     flexShrink: 0,
                     '& .MuiDrawer-paper': {
-                        // width: 160,
-                        // backgroundColor: " green"
+                        width: drawerWidth,
+                        backgroundColor: green[400],
+                        color: "white",
                     },
                 }}
                 variant="persistent"
@@ -41,38 +68,53 @@ export default () => {
             >
                 <DrawerHeader>
                     <IconButton onClick={handleDrawerClose}>
-                        <ChevronLeftIcon />
+                        <ChevronLeftIcon style={{ color: "white" }} />
                     </IconButton>
                 </DrawerHeader>
-                <Divider />
+                <Divider style={{ border: "0.5px solid white" }} />
                 <List>
-                    <ListItem>
-                        Dynamic Programming
+                    <ListItem disablePadding>
+                        <ListItemButton style={{ paddingLeft: 0, paddingRight: 0 }}>
+                            <Typography variant="body1" align='center' style={{ width: "100%" }}>
+                                Dynamic Programming
+                            </Typography>
+                        </ListItemButton>
                     </ListItem>
-                    <ListItem>
-                        Sorting
+                    <ListItem disablePadding>
+                        <ListItemButton style={{ paddingLeft: 0, paddingRight: 0 }}>
+                            <Typography variant="body1" align='center' style={{ width: "100%" }}>
+                                Sorting
+                            </Typography>
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton style={{ paddingLeft: 0, paddingRight: 0 }}>
+                            <Typography variant="body1" align='center' style={{ width: "100%" }}>
+                                Tree
+                            </Typography>
+                        </ListItemButton>
                     </ListItem>
                 </List>
             </Drawer>
-            <div style={{
-                width: "100%",
-                // backgroundColor: "red"
-            }}>
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                    // sx={{ mr: 2, ...(open && { display: 'none' }) }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
+            <Main open={true}  >
+                hello world
+            </Main>
+            <Toolbar>
+                <IconButton
+                    color="inherit"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    sx={{
+                        mr: 2, ...(open && { display: 'none' }),
+                        position: "fixed",
+                        top: 10,
+                        border: "1px solid"
+                    }}
+                >
+                    <MenuIcon />
+                </IconButton>
 
-                    <Typography variant="h6">
-                        alchemist
-                    </Typography>
-                </Toolbar>
-            </div>
+            </Toolbar>
         </>
 
     );
