05e460024589dd6d64840999352551e69011299a
Dong Zhou <dozhou@rubiconproject.com>
2022-05-16 01:55:26 -0700

    Update Side.tsx

--- src/components/Side.tsx
+++ src/components/Side.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import MaterialLink from "@mui/material/Link";
-import { styled, useTheme } from '@mui/material/styles';
-import { Divider, Drawer, IconButton, List, ListItem, ListItemButton, Toolbar, Typography } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import { Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography } from "@mui/material";
 import { ChevronLeft, Menu } from '@mui/icons-material';
 
-const bottom = 5;
+const bottom = 0;
 
 const ChevronPosition = styled("div")<{
     drawerWidth: number
@@ -28,6 +28,7 @@ const CloseDrawer: React.FC<{
     setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }> = ({ drawerWidth, setOpen }) => (
     <ChevronPosition drawerWidth={drawerWidth}>
+        <Divider />
         <ChevronHolder>
             <IconButton onClick={() => setOpen(false)}>
                 <ChevronLeft color="primary" />
@@ -50,12 +51,6 @@ const OpenDrawer: React.FC<{
     </Toolbar>
 );
 
-interface Props {
-    drawerWidth: number;
-    open: boolean;
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
 const Logo = () => (
     <Toolbar style={{ overflow: "hidden", padding: 0 }}>
         <Typography variant="h6" align="center" style={{ marginTop: 6, marginBottom: 6, width: "100%" }} color="primary">
@@ -64,65 +59,56 @@ const Logo = () => (
     </Toolbar>
 );
 
-export default ({ drawerWidth, open, setOpen }: Props) => {
-
-    const theme = useTheme();
-
-    return (
-        <>
-            <Drawer
-                sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    '& .MuiDrawer-paper': {
-                        width: drawerWidth,
-                    },
-                }}
-                variant="persistent"
-                anchor="left"
-                open={open}
+const Item: React.FC<{ name: string, path: string }> = ({ name, path }) => (
+    <ListItem disablePadding>
+        <ListItemButton>
+            <MaterialLink
+                component={RouterLink}
+                to={path}
+                style={{ textDecoration: 'none', textAlign: 'left', width: "100%" }}
+                color="secondary"
             >
-                <Logo />
+                <ListItemText primary={name} />
+            </MaterialLink>
+        </ListItemButton>
+    </ListItem>
+);
 
-                <Divider />
-                <List sx={{ padding: 0 }}>
-                    <ListItem disablePadding>
-                        <ListItemButton sx={{ paddingLeft: 0, paddingRight: 0 }}>
-                            <MaterialLink
-                                component={RouterLink}
-                                to={"/home"}
-                                style={{ textDecoration: 'none', width: "100%", textAlign: 'center' }}
-                                color="secondary"
-                            >
-                                Dynamic Programming
-                            </MaterialLink>
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemButton sx={{ paddingLeft: 0, paddingRight: 0 }}>
-                            <Typography variant="body2" align='center' sx={{ width: "100%" }}
-                                color="secondary"
-                            >
-                                Sorting
-                            </Typography>
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemButton sx={{ paddingLeft: 0, paddingRight: 0 }}>
-                            <Typography variant="body2" align='center' sx={{ width: "100%" }}
-                                color="secondary">
-                                Tree
-                            </Typography>
-                        </ListItemButton>
-                    </ListItem>
-                </List>
+const Items = () => (
+    <List sx={{ padding: 0 }}>
+        <Item name="Dynamic Programming" path="dynamic-programming" />
+        <Item name="Sorting" path="sorting" />
+        <Item name="Tree" path="tree" />
+    </List>
+);
 
-                <CloseDrawer drawerWidth={drawerWidth} setOpen={setOpen} />
-            </Drawer>
-
-            <OpenDrawer open={open} setOpen={setOpen} />
-        </>
-
-    );
-
+interface Props {
+    drawerWidth: number;
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
+export default ({ drawerWidth, open, setOpen }: Props) => (
+    <>
+        <Drawer
+            variant="persistent"
+            anchor="left"
+            open={open}
+            sx={{
+                width: drawerWidth,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    width: drawerWidth,
+                },
+            }}
+        >
+            <Logo />
+            <Divider />
+            <Items />
+            <CloseDrawer drawerWidth={drawerWidth} setOpen={setOpen} />
+        </Drawer>
+
+        <OpenDrawer open={open} setOpen={setOpen} />
+    </>
+
+);
