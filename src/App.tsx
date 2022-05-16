5313fa16bd909052cc6ae5ddb438edd8202d8a1d
Dong Zhou <dozhou@rubiconproject.com>
2022-05-15 20:06:10 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import { Divider, IconButton, List, ListItem, Toolbar, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import MenuIcon from '@mui/icons-material/Menu';
 import Side from './components/Side';
 import Main from "./components/Main";
 
@@ -15,7 +10,7 @@ function App() {
   const [open, setOpen] = React.useState<boolean>(true);
 
   return (
-    <Box sx={{ display: "flex" }} style={{ height: "100%" }}>
+    <Box sx={{ display: "flex", height: "100%" }}>
       <Side drawerWidth={drawerWidth} open={open} setOpen={setOpen} />
       <Main drawerWidth={drawerWidth} open={open} />
     </Box>
